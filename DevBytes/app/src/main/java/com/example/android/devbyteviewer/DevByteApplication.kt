8e5d8edb5e9547703e8a9fa1c33e14d8e6b40876
jc <luvimperfection@gmail.com>
2021-04-14 21:01:06 +0900

    Implement prefetching
    Create Worker and make a periodic work request. Define constraints for the request.

--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -18,12 +18,23 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
  */
+
+// Application() --> OS uses this to interact with the app
 class DevByteApplication : Application() {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -31,8 +42,58 @@ class DevByteApplication : Application() {
      * Use it to setup any background tasks, running expensive setup operations in a background
      * thread to avoid delaying app start.
      */
+    // runs every time the app launches!!!
     override fun onCreate() {
         super.onCreate()
+        // Configure logging in the app
         Timber.plant(Timber.DebugTree())
+        // Can check what happens.. --> should do the least work inside onCreate!!!
+//        Thread.sleep(4_000_000)
+
+        delayedInit()
+    }
+    // Run some long-running process in the background, using coroutine
+    private fun delayedInit(){
+        applicationScope.launch {
+            // Inside here,  delay doesn't happen in initialization, just before launch step!
+//            Thread.sleep(4_000_000)
+            setupRecurringWork()
+
+        }
+    }
+
+    private fun setupRecurringWork() {
+        // define constraints
+        val constraints = Constraints.Builder()
+                // Use network in unmetered situation (Ex. wifi zone..)
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                // require that the device is in charging state
+                .setRequiresCharging(true)
+                .apply{
+                    // for higher versions
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        // do the background thing when the device is idle
+                        setRequiresDeviceIdle(true)
+                    }
+                }.build()
+
+        // build work request
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
+                1,
+                TimeUnit.DAYS
+        )
+                .setConstraints(constraints)
+                .build()
+        // schedule the work
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+                // work should be unique
+                RefreshDataWorker.WORK_NAME,
+                // what if not unique? --> policy
+                // KEEP --> keep the previous work request & discard the new one
+                // REPLACE --> replace the old request
+                ExistingPeriodicWorkPolicy.KEEP,
+                repeatingRequest
+        )
     }
 }
--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,32 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+        CoroutineWorker(appContext, params){
+    companion object{
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+
+    // Worker will work until doWork() returns result
+    // Worker will make sure that the operation system do not interrupt the work written here
+    // doWork will run in background thread not in ui thread
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try{
+            repository.refreshVideos()
+            Result.success()
+        }catch (e: HttpException){
+            Result.retry()
+        }
+    }
+}
